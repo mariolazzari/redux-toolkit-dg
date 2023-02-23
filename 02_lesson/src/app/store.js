c0246387f0acc2e1fbffea7975bb09fa7061b60d
Mario Lazzari <mario.lazzari@gmail.com>
2023-02-23 21:08:20 +0100

    Done lesson 2

--- 02_lesson/src/app/store.js
+++ 02_lesson/src/app/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import post from "../features/posts/postsSlice";
+import postsReducer from '../features/posts/postsSlice';
+import usersReducer from '../features/users/usersSlice';
+
 
 export const store = configureStore({
-  reducer: { post },
-});
+    reducer: {
+        posts: postsReducer,
+        users: usersReducer
+    }
+})
--- 02_lesson/src/index.js
+++ 02_lesson/src/index.js
@@ -1,13 +1,15 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
